15c699b7b4b573f1243d750d64a2aa6f79c99f79
Islam Sayed <islam.sayed8@gmail.com>
2017-12-07 16:18:47 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,23 +1,35 @@
-
 /* User select size of canvas.
-*Then click submit.
-*and this call makeGrid()
-*/
-$('#sizePicker').submit(function () {     
+ *Then click submit.
+ *and this call makeGrid()
+ */
+$('.makeButton').click(function() {
     makeGrid();
-    return false;   
-});
-$('#gridClearer').submit(function (){
-    clearGrid();
     return false;
 });
- 
-// Select color input
-$('#pixel_canvas').on('click' ,'td' ,function () { 
-    const color = $('#colorPicker').val();
-    $(this).attr('bgcolor', color);    
+
+
+
+//Clear cell by double clicking cell.
+
+$('#pixel_canvas').on('dblclick', 'td', function() {
+    $(this).toggleClass('cellColor');
 });
 
+
+
+// Select color input
+$('#pixel_canvas').on('click', 'td', function() {
+    var color = $('#colorPicker').val();
+    $(this).attr('bgcolor', color);
+});
+
+
+
+
+$('#colorPicker').on('change', function() {
+    var z = $('#colorPicker').val();
+    $('#pickColorText').css('color', z);
+});
 /*$('#pixel_canvas').on('mousedown', 'td', function(){
   *  var color = $('#colorPicker').val();
   *  $(this).attr('bgcolor', color);
@@ -25,27 +37,20 @@ $('#pixel_canvas').on('click' ,'td' ,function () {
 
 
 /*When size is submitted by the user, call makeGrid()
-*This removes the previous colored canvas if present.
-*and returns a new one with the new values of height and width eneterd by the user.
-*/
-
+ *This removes the previous colored canvas if present.
+ *and returns a new one with the new values of height and width eneterd by the user.
+ */
 
 function makeGrid() {
     $('tr').remove();
     $('td').remove();
-    $('#colorPicker').val("#000000");
-    const height = $('#input_height').val();
-    const width = $('#input_width').val();
-    for(let x = 1; x <= height; x++){
+
+    var height = $('#input_height').val();
+    var width = $('#input_width').val();
+    for (let x = 1; x <= height; x++) {
         $('#pixel_canvas').append('<tr></tr>');
-        for(let y = 1; y <= width; y++){
+        for (let y = 1; y <= width; y++) {
             $("#pixel_canvas").children().last().append('<td></td>');
         }
     }
-}
-
-function clearGrid() {
-
-    $('td').attr('bgcolor' ,"#ffffff");
-
 }
